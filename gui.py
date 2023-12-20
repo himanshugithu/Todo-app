d214ebb9dc1ea3e37faecb14fa23e1f3a53db64c
himanshugithu <fanibharehimanshu@gmail.com>
2023-12-21 00:12:51 +0530

    gui change

--- gui.py
+++ gui.py
@@ -1,9 +1,16 @@
 import PySimpleGUI as sg
+import function
 lable = sg.Text("type in todo")
 input_box = sg.InputText(tooltip="enter todo",key ="todo")
 add_button = sg.Button("Add") 
 window = sg.Window("my todo app",layout=[[lable],[input_box,add_button]])
 while True:
-    event = window.read()
-    print(event[1])
+    event,value = window.read()
+    match event:
+        case 'add':
+            todos = function.get_todos()
+            new_todo = value['todo'] + '\n'
+            todos.append(new_todo)
+            function.write_todos(todos)
+    
 window.close()
