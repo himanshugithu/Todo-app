260c745aa4218675c24309c9a96d201d6cf02f05
himanshugithu <fanibharehimanshu@gmail.com>
2023-12-21 19:24:29 +0530

    21/12/23(1)

--- gui.py
+++ gui.py
@@ -1,9 +1,9 @@
 import PySimpleGUI as sg
 import function
 lable = sg.Text("type in todo")
-input_box = sg.InputText(tooltip="enter todo",key ="todo")
+input_box = sg.InputText(tooltip="enter todo",key ="input")
 add_button = sg.Button("Add") 
-list_box = sg.Listbox(values=function.get_todos(),key = 'todos',enable_events=True,size=[45,10])
+list_box = sg.Listbox(values=function.get_todos(),key = 'list_box',enable_events=True,size=[45,10])
 edit_button = sg.Button('Edit')
 
 window = sg.Window("my todo app",layout=[[lable],[input_box,add_button],[list_box,edit_button]])
@@ -13,13 +13,22 @@ while True:
     match event:
         case 'Add':
             todos = function.get_todos()
-            new_todo = values['todo'] + '\n'
-            print(values['todo'])
+            new_todo = values['input'] + '\n'
+            print(values['input'])
             todos.append(new_todo)
             function.write_todos(todos)
+            window['list_box'].update(values=todos)
         case 'Edit':
-            todo_to_edit     = values['todos'][0]
-            print(values['todos'])   
+            todo_to_edit = values['list_box'][0]
+            new_todo = values['input']
+            todos = function.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            function.write_todos(todos)
+            window['list_box'].update(values=todos)
+
+        case 'list_box':
+            window["input"].update(value=values['list_box'][0])    
         case sg.WIN_CLOSED:
             break
 window.close()
